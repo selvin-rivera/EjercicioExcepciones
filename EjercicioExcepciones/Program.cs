c90e3f18897596ed590c1f1c7e47d618721d0659
selvin-rivera <nrivera.clements@gmail.com>
2024-05-19 17:26:41 -0600

    creando clase y metodos

--- EjercicioExcepciones/Program.cs
+++ EjercicioExcepciones/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioExcepciones
+
+    
+     static void Menu()
+    {
+        Console.WriteLine("\nSeleccione una opción:");
+        Console.WriteLine("1. Sumar");
+        Console.WriteLine("2. Restar");
+        Console.WriteLine("3. Multiplicar");
+        Console.WriteLine("4. Dividir");
+        Console.WriteLine("5. Salir");
+    }
+
